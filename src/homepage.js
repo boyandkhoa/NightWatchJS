8614efa1e34039887714550e808df27870700eb5
Your Name <you@example.com>
2017-10-02 21:54:29 +0700

    Add all file

---
+++ src/homepage.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'Demo test Google' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', ['nightwatch', browser.Keys.ENTER]);
+  }
+};
